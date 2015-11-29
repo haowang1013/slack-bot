c4b402a0f35790ed573bb36e6aeaaecf12e81292
Wang Hao <wanghao@directivegames.com>
2015-11-29 12:37:38 +0800

    Improve the static data commands:
    - Rename command "index" to "commits", embed the types and schemas url in the response
    - Merge command "types" and "schemas" to "commit", which returns the url for both resources

--- commands/staticData.go
+++ commands/staticData.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/haowang1013/slack-bot/utils"
@@ -12,39 +13,61 @@ const (
 	dataRootUrl = "https://d1pqpvg8ar5xgy.cloudfront.net/directivegames/the-machines-static-data/data/"
 )
 
+type staticDataEntry struct {
+	CommitID string `json:"commit_id"`
+	Ref      string `json:"ref"`
+	Types    string `json:"types"`
+	Schemas  string `json:"schemas"`
+}
+
+type staticDataRoot struct {
+	Entries []staticDataEntry `json:"index"`
+}
+
 func init() {
 	rootHandler.addHandler("sd", &handleCollection{
-		"index":   HandlerFunc(indexHandler),
-		"types":   HandlerFunc(typesHandler),
-		"schemas": HandlerFunc(schemasHandler),
+		"commits": HandlerFunc(commitsHandler),
+		"commit":  HandlerFunc(commitHandler),
 	})
 }
 
-func indexHandler(fields []string, m *slack.MessageEvent) error {
+func commitsHandler(fields []string, m *slack.MessageEvent) error {
 	resp, err := utils.HttpGet(indexUrl)
 	if err != nil {
 		return err
 	}
-	sendMessage(string(resp), m.Channel)
+
+	var root staticDataRoot
+	err = json.Unmarshal(resp, &root)
+	if err != nil {
+		return err
+	}
+
+	for i := range root.Entries {
+		elem := &root.Entries[i]
+		elem.Types, elem.Schemas = getDataUrl(elem.CommitID)
+		buff, err := json.MarshalIndent(elem, "", " ")
+		if err != nil {
+			return err
+		}
+		sendMessage(string(buff), m.Channel)
+	}
 	return nil
 }
 
-func typesHandler(fields []string, m *slack.MessageEvent) error {
+func getDataUrl(commitID string) (types string, schemas string) {
+	types = fmt.Sprintf("%s%s/types.json", dataRootUrl, commitID)
+	schemas = fmt.Sprintf("%s%s/schemas.json", dataRootUrl, commitID)
+	return
+}
+
+func commitHandler(fields []string, m *slack.MessageEvent) error {
 	if len(fields) == 0 {
 		return errors.New("Missing commit id")
 	}
 
-	url := fmt.Sprintf("%s%s/types.json", dataRootUrl, fields[0])
-	sendMessage(url, m.Channel)
-	return nil
-}
-
-func schemasHandler(fields []string, m *slack.MessageEvent) error {
-	if len(fields) == 0 {
-		return errors.New("Missing commit id")
-	}
-
-	url := fmt.Sprintf("%s%s/schemas.json", dataRootUrl, fields[0])
-	sendMessage(url, m.Channel)
+	types, schemas := getDataUrl(fields[0])
+	msg := fmt.Sprintf("types: %s\nschemas: %s", types, schemas)
+	sendMessage(msg, m.Channel)
 	return nil
 }
